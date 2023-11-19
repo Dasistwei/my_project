b03fcff357ac69eaa186d92598d899e58deaea4c
wei <wei02919@gmail.com>
2023-11-19 15:56:12 +0800

    add folder

---
+++ clock/script.js
@@ -0,0 +1,10 @@
+let clock = document.getElementById("clock")
+// clock.textContent = new Date()
+// let timer= setInterval(()=>{
+//   let hours = new Date().getHours()
+//   let minutes= new Date().getMinutes()
+//   let seconds= new Date().getSeconds()
+//   let time = hours + ':' + minutes + ':' + seconds
+//   clock.textContent = time
+//   return console.log(time) 
+// }, 1000)
